68ce2b9abf2f7778c6da4ac2a07712719e1eec3e
Kevin Delemme <kdelemme@gmail.com>
2014-05-26 21:32:18 +0200

    Add Less (issue #15)

---
+++ app/js/directives.js
@@ -0,0 +1,18 @@
+appDirectives.directive('displayMessage', function() {
+	return {
+		restrict: 'E',
+		scope: {
+        	messageType: '=type',
+        	message: '=data'
+      	},
+		template: '<div class="alert {{messageType}}">{{message}}</div>',
+		link: function (scope, element, attributes) {
+            scope.$watch(attributes, function (value) {
+            	console.log(attributes);
+            	console.log(value);
+            	console.log(element[0]);
+                element[0].children.hide(); 
+            });
+        }
+	}
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var less = require('gulp-less');
 
 var paths = {
-    scripts: ['app/js/**/*.js']
+    scripts: ['app/js/**/*.js'],
+    styles: ['app/css/style.less'] 
 };
 
 gulp.task('scripts', function() {
     // concat and copy all JavaScript
     return gulp.src(paths.scripts)
         .pipe(concat('blog.js'))
-        .pipe(gulp.dest('app/dist'));
+        .pipe(gulp.dest('app/dist/js'));
 });
 
+gulp.task('styles', function() {
+	return gulp.src(paths.styles)
+		.pipe(less())
+		.pipe(gulp.dest('app/dist/css'));
+})
+
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts']); 
+gulp.task('default', ['scripts', 'styles']); 
