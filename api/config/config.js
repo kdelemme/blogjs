cdf5b5a6bfdb31bf570215947c3939358db82d49
Arnaud SAMSON <nono.samson@gmail.com>
2014-08-12 16:15:36 +0200

    fix bug with rss feed

--- api/config/config.js
+++ api/config/config.js
@@ -8,7 +8,7 @@ config.rss = {
   description:  'LoremIpsum',
   link:         'http://mySite.fr',
   image:        'http://mySite.fr/logo.png',
-  copyright     'Copyright © 2013 John Doe. All rights reserved'
+  copyright:     'Copyright © 2013 John Doe. All rights reserved'
 };
 
 module.exports = config;
